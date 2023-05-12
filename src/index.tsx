0f1ceee66fcd4e8e36a4dd9a5b4ca19bcca69b80
OksanaPopovichenko <oksana@synaps.io>
2023-05-12 17:46:44 +0200

    :bug: Fixed deploy

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider>
-      <BrowserRouter>
+      <BrowserRouter basename="/Box-RDV">
         <Routes>
           <Route path="/" element={<App />} />
         </Routes>
